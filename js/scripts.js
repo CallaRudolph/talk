027a8641fdd15eb2a72fdd086eea6017c191fa81
Brittany Kerr and Calla Rudolph <brittany@>
2017-05-31 11:02:33 -0700

    add remove from first child

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
     $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#webpage").children("li").first().click(function(){
+      $(this).remove();
+    });
+
   });
 
   $("button#goodbye").click(function() {
